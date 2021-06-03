5024552ce70c7d376f6d5c9178e4b251590c6414
diegoejp <d.jacobpulgar@gmail.com>
2021-06-03 11:12:58 -0400

    carta lista con boton

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let btn = document.querySelector("#boton");
+  btn.addEventListener("click", () => {
+    //
+    let centro = document.querySelector("#medio");
+    let abajo = document.querySelector("#abajo");
+    let arriba = document.querySelector("#arriba");
+    centro.innerHTML = numeral();
+    let icon = icono();
+    if (icon == "♥" || icon == "♦") {
+      abajo.classList.add("red");
+      arriba.classList.add("red");
+      centro.classList.add("red");
+    } else {
+      abajo.classList.remove("red");
+      arriba.classList.remove("red");
+      centro.classList.remove("red");
+    }
+    abajo.innerHTML = icon;
+    arriba.innerHTML = icon;
+    console.log(numeral());
+    //
+  });
 };
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+let simbolos = ["♥", "♦", "♠", "♣"];
+//generar icono
+function icono() {
+  return simbolos[Math.floor(Math.random() * 4)];
+}
+function numeral() {
+  return numeros[Math.floor(Math.random() * 13)];
+}
